7681b7ac1851aca6896eadf04b16b7aa9e047980
zach <zach@dylibso.com>
2025-01-07 15:24:39 -0800

    feat: extend example

--- example.py
+++ example.py
@@ -1,8 +1,9 @@
 from mcpx_pydantic_ai import Agent, BaseModel, Field
 
-import code
 from typing import List
 import readline
+
+from pydantic_ai import capture_run_messages
 
 
 class ImageList(BaseModel):
@@ -21,21 +22,40 @@ def new_agent(result_type, model="claude-3-5-sonnet-latest"):
     return Agent(model, result_type=result_type)
 
 
+history = []
+
+
 def run(agent, msg):
     """
     Send a message to an agent and return the result
     """
-    result = agent.run_sync(msg)
+    global history
+    with capture_run_messages() as messages:
+        result = agent.run_sync(msg, message_history=history)
+        history.extend(messages)
     return result.data
 
 
-agent = new_agent(ImageList | VowelCount)
+types = ImageList | VowelCount
+agent = new_agent(types)
 
 while True:
     msg = input("> ")
+    readline.add_history(msg)
     if msg == "exit":
         break
     elif len(msg) == 0:
+        continue
+    elif msg.startswith("/type"):
+        msg = msg[5:].strip()
+        name = msg.split(" ", maxsplit=1)[0]
+        s = msg[len(name) + 1 :]
+        exec(f"""
+class Type_{name}(BaseModel):
+    {s}
+        """)
+        t = eval(f"Type_{name}")
+        agent = new_agent(types | t)
         continue
     res = run(agent, msg)
     print(">>", res)
